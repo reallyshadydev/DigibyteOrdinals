18fdf474b242251853adc9b94c878d644e595e53
root <martin.seeger2002@gmail.com>
2025-01-11 12:18:19 -0600

    Add DigiByte library and assets: bitcore-lib-digibyte, digibear.jpg, unnamed.png

---
+++ bitcore-lib-digibyte/lib/networks.js
@@ -0,0 +1,185 @@
+'use strict';
+var _ = require('lodash');
+
+var BufferUtil = require('./util/buffer');
+var JSUtil = require('./util/js');
+var networks = [];
+var networkMaps = {};
+
+/**
+ * A network is merely a map containing values that correspond to version
+ * numbers for each digibyte network. Currently only supporting "livenet"
+ * (a.k.a. "mainnet"), "testnet", and "regtest".
+ * @constructor
+ */
+function Network() {}
+
+Network.prototype.toString = function toString() {
+  return this.name;
+};
+
+/**
+ * @function
+ * @member Networks#remove
+ * Will remove a custom network
+ * @param {Network} network
+ */
+function removeNetwork(network) {
+  if (typeof network !== 'object') {
+    network = get(network);
+  }
+  for (var i = 0; i < networks.length; i++) {
+    if (networks[i] === network) {
+      networks.splice(i, 1);
+    }
+  }
+  for (var key in networkMaps) {
+    if (networkMaps[key].length) {
+      const index = networkMaps[key].indexOf(network);
+      if (index >= 0) {
+        networkMaps[key].splice(index, 1);
+      }
+      if (networkMaps[key].length === 0) {
+        delete networkMaps[key];
+      }
+    } else if (networkMaps[key] === network) {
+      delete networkMaps[key];
+    }
+  }
+}
+
+/**
+ * @function
+ * @member Networks#get
+ * Retrieves the network associated with a magic number or string.
+ * @param {string|number|Network} arg
+ * @param {string|Array} keys - if set, only check if the magic number associated with this name matches
+ * @return Network
+ */
+function get(arg, keys) {
+  if (~networks.indexOf(arg)) {
+    return arg;
+  }
+  if (keys) {
+    if (!_.isArray(keys)) {
+      keys = [keys];
+    }
+    var containsArg = function(key) {
+      return networks[index][key] === arg;
+    };
+    for (var index in networks) {
+      if (_.some(keys, containsArg)) {
+        return networks[index];
+      }
+    }
+    return undefined;
+  }
+  if(networkMaps[arg] && networkMaps[arg].length >= 1) {
+    return networkMaps[arg][0];
+  } else {
+    return networkMaps[arg];
+  }
+}
+
+/**
+ * @function
+ * @member Networks#add
+ * Will add a custom Network
+ * @param {Object} data
+ */
+function addNetwork(data) {
+  var network = new Network();
+  JSUtil.defineImmutable(network, {
+    name: data.name,
+    alias: data.alias,
+    pubkeyhash: data.pubkeyhash,
+    privatekey: data.privatekey,
+    scripthash: data.scripthash,
+    xpubkey: data.xpubkey,
+    xprivkey: data.xprivkey,
+    networkMagic: data.networkMagic ? BufferUtil.integerAsBuffer(data.networkMagic) : undefined,
+    port: data.port,
+    dnsSeeds: data.dnsSeeds
+  });
+
+  _.each(network, function(value) {
+    if (!_.isUndefined(value) && !_.isObject(value)) {
+      if(!networkMaps[value]) {
+        networkMaps[value] = [];
+      }
+      networkMaps[value].push(network);
+    }
+  });
+  networks.push(network);
+  return network;
+}
+
+// Add DigiByte Mainnet
+addNetwork({
+  name: 'livenet',
+  alias: 'mainnet',
+  pubkeyhash: 0x1e,
+  privatekey: 0x80,
+  scripthash: 0x3f,
+  xpubkey: 0x0488b21e,
+  xprivkey: 0x0488ade4,
+  networkMagic: 0xfac3b6da,
+  port: 12024,
+  dnsSeeds: [
+    'seed.digibyte.io',
+    'seed2.digibyte.io',
+    'seed3.digibyte.io',
+    'seed.digibyteprojects.com'
+  ]
+});
+
+var livenet = get('livenet');
+
+// Add DigiByte Testnet
+addNetwork({
+  name: 'testnet',
+  alias: 'test',
+  pubkeyhash: 0x6f,
+  privatekey: 0xef,
+  scripthash: 0xc4,
+  xpubkey: 0x043587cf,
+  xprivkey: 0x04358394,
+  networkMagic: 0xfdd2c8f1,
+  port: 12025,
+  dnsSeeds: [
+    'testnet-seed.digibyte.io',
+    'testnet-seed.digibyteprojects.com'
+  ]
+});
+
+var testnet = get('testnet');
+
+// Add DigiByte Regtest
+addNetwork({
+  name: 'regtest',
+  alias: 'dev',
+  pubkeyhash: 0x6f,
+  privatekey: 0xef,
+  scripthash: 0xc4,
+  xpubkey: 0x043587cf,
+  xprivkey: 0x04358394,
+  networkMagic: 0xdab5bffa,
+  port: 18444,
+  dnsSeeds: []
+});
+
+var regtest = get('regtest');
+
+/**
+ * @namespace Networks
+ */
+module.exports = {
+  add: addNetwork,
+  remove: removeNetwork,
+  defaultNetwork: livenet,
+  livenet: livenet,
+  mainnet: livenet,
+  testnet: testnet,
+  regtest: regtest,
+  get: get
+};
